8e1e99ae8ec190372e1277ff54f91da7ff3c813d
heather4842 <heather4842@naver.com>
2025-08-21 17:27:00 +0900

    08.21 쇼핑몰 완성
    와~

--- src/main/java/com/shop/entity/CartItem.java
+++ src/main/java/com/shop/entity/CartItem.java
@@ -1,23 +1,21 @@
 package com.shop.entity;
 
-
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.persistence.*;
 
 @Entity
-@Getter
-@Setter
-@Table(name="cartItem")
+@Getter @Setter
+@Table(name="cart_item")
+public class CartItem extends BaseEntity {
 
-public class CartItem extends BaseEntity{
     @Id
     @GeneratedValue
     @Column(name = "cart_item_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cart_id")
+    @JoinColumn(name="cart_id")
     private Cart cart;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -25,7 +23,6 @@ public class CartItem extends BaseEntity{
     private Item item;
 
     private int count;
-
 
     public static CartItem createCartItem(Cart cart, Item item, int count) {
         CartItem cartItem = new CartItem();
@@ -35,8 +32,12 @@ public class CartItem extends BaseEntity{
         return cartItem;
     }
 
-    public void addCount(int count) {
+    public void addCount(int count){
         this.count += count;
+    }
+
+    public void updateCount(int count){
+        this.count = count;
     }
 
 }
--- src/main/java/com/shop/repository/Item/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shop.repository.Item;
-
-import com.shop.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
-    CartItem findByCartIdAndItemId(Long cartId, Long itemId);
-}
-
